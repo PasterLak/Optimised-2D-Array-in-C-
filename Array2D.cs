f063063435a804606d8e00f7e384ff000527b51a
Vladislav Bezborodyi <59233654+PasterLak@users.noreply.github.com>
2023-01-10 00:47:58 +0100

    Added file

---
+++ Array2D.cs
@@ -0,0 +1,46 @@
+using System;
+
+public struct Array2D<T>
+{
+    private T[] _data;
+
+    private int _width;
+    private int _height;
+
+    public int Width => _width;
+    public int Height => _height;
+
+    public Array2D(int width, int height)
+    {
+        _data = new T[width * height];
+        _width = width;
+        _height = height;
+    }
+    
+    public T this[int width, int height]
+    {
+        get => _data[GetIndex(width, height)];
+        set => _data[GetIndex(width, height)] = value;
+    }
+
+    private int GetIndex(int x, int y) => x * _height + y;
+
+    public override string ToString()
+    {
+        var s = String.Empty;
+
+        for (int i = 0; i < _width; i++)
+        {
+            s += "\n";
+            
+            for (int x = 0; x < _height; x++)
+            {
+                s += $"{_data[GetIndex(i,x)]},";
+            }
+
+        }
+
+        return s;
+    }
+}
+ 
